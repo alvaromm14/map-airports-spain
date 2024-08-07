e0adf24db7c1a28ab1637aa43265c1ae6891f601
alvaromm14 <49958868+alvaromm14@users.noreply.github.com>
2024-08-07 17:49:48 +0200

    Add files via upload

---
+++ src/data/aeropuertos.js
@@ -0,0 +1,386 @@
+export default [
+    {
+      "AEROPUERTOS": "Adolfo Suárez Madrid-Barajas",
+      "Latitude": 40.4895147,
+      "Longitude": -3.5642764,
+      "Total": 60220984,
+      "Cambio2022": 0.19,
+      "Cambio2019": -0.02
+    },
+    {
+      "AEROPUERTOS": "Barcelona-El Prat Josep Tarradellas",
+      "Latitude": 41.2983404999999,
+      "Longitude": 2.0800095,
+      "Total": 49909544,
+      "Cambio2022": 0.20,
+      "Cambio2019": -0.05
+    },
+    {
+      "AEROPUERTOS": "Palma de Mallorca",
+      "Latitude": 39.552463,
+      "Longitude": 2.7393901,
+      "Total": 31105987,
+      "Cambio2022": 0.09,
+      "Cambio2019": 0.05
+    },
+    {
+      "AEROPUERTOS": "Málaga-Costa del Sol",
+      "Latitude": 36.6767427,
+      "Longitude": -4.4938696,
+      "Total": 22344373,
+      "Cambio2022": 0.21,
+      "Cambio2019": 0.13
+    },
+    {
+      "AEROPUERTOS": "Alicante-Elche Miguel Hernández",
+      "Latitude": 38.284924,
+      "Longitude": -0.5622132,
+      "Total": 15747678,
+      "Cambio2022": 0.19,
+      "Cambio2019": 0.05
+    },
+    {
+      "AEROPUERTOS": "Gran Canaria",
+      "Latitude": 27.9292098,
+      "Longitude": -15.3872261,
+      "Total": 13961638,
+      "Cambio2022": 0.12,
+      "Cambio2019": 0.05
+    },
+    {
+      "AEROPUERTOS": "Tenerife Sur",
+      "Latitude": 28.0467575,
+      "Longitude": -16.5725303,
+      "Total": 12337244,
+      "Cambio2022": 0.14,
+      "Cambio2019": 0.10
+    },
+    {
+      "AEROPUERTOS": "Valencia",
+      "Latitude": 39.4883525,
+      "Longitude": -0.484433,
+      "Total": 9948141,
+      "Cambio2022": 0.23,
+      "Cambio2019": 0.16
+    },
+    {
+      "AEROPUERTOS": "Ibiza",
+      "Latitude": 38.8739301,
+      "Longitude": 1.3722454,
+      "Total": 8931598,
+      "Cambio2022": 0.10,
+      "Cambio2019": 0.10
+    },
+    {
+      "AEROPUERTOS": "Lanzarote-César Manrique",
+      "Latitude": 28.950438,
+      "Longitude": -13.6057769,
+      "Total": 8212943,
+      "Cambio2022": 0.12,
+      "Cambio2019": 0.13
+    },
+    {
+      "AEROPUERTOS": "Sevilla",
+      "Latitude": 37.4202658,
+      "Longitude": -5.8907912,
+      "Total": 8071524,
+      "Cambio2022": 0.19,
+      "Cambio2019": 0.07
+    },
+    {
+      "AEROPUERTOS": "Bilbao",
+      "Latitude": 43.3022693,
+      "Longitude": -2.9154643,
+      "Total": 6336441,
+      "Cambio2022": 0.24,
+      "Cambio2019": 0.07
+    },
+    {
+      "AEROPUERTOS": "Tenerife Norte-Ciudad de la Laguna",
+      "Latitude": 28.484511,
+      "Longitude": -16.3434204,
+      "Total": 6120550,
+      "Cambio2022": 0.10,
+      "Cambio2019": 0.05
+    },
+    {
+      "AEROPUERTOS": "Fuerteventura",
+      "Latitude": 28.4482157,
+      "Longitude": -13.866349,
+      "Total": 6020413,
+      "Cambio2022": 0.07,
+      "Cambio2019": 0.07
+    },
+    {
+      "AEROPUERTOS": "Menorca",
+      "Latitude": 39.8633392,
+      "Longitude": 4.2211303,
+      "Total": 4045215,
+      "Cambio2022": 0.04,
+      "Cambio2019": 0.16
+    },
+    {
+      "AEROPUERTOS": "Santiago-Rosalía de Castro",
+      "Latitude": 42.8920814,
+      "Longitude": -8.41959389999999,
+      "Total": 3537445,
+      "Cambio2022": 0.09,
+      "Cambio2019": 0.22
+    },
+    {
+      "AEROPUERTOS": "Asturias",
+      "Latitude": 43.5623825,
+      "Longitude": -6.0334978,
+      "Total": 1974850,
+      "Cambio2022": 0.36,
+      "Cambio2019": 0.39
+    },
+    {
+      "AEROPUERTOS": "Girona-Costa Brava",
+      "Latitude": 41.8978423999999,
+      "Longitude": 2.7660641,
+      "Total": 1586463,
+      "Cambio2022": 0.21,
+      "Cambio2019": -0.18
+    },
+    {
+      "AEROPUERTOS": "La Palma",
+      "Latitude": 28.625881,
+      "Longitude": -17.7551131,
+      "Total": 1368821,
+      "Cambio2022": 0.05,
+      "Cambio2019": -0.08
+    },
+    {
+      "AEROPUERTOS": "A Coruña",
+      "Latitude": 43.3012532,
+      "Longitude": -8.37879139999999,
+      "Total": 1252022,
+      "Cambio2022": 0.30,
+      "Cambio2019": -0.07
+    },
+    {
+      "AEROPUERTOS": "Seve Ballesteros-Santander",
+      "Latitude": 43.4263098,
+      "Longitude": -3.821079,
+      "Total": 1242089,
+      "Cambio2022": 0.13,
+      "Cambio2019": 0.06
+    },
+    {
+      "AEROPUERTOS": "Vigo",
+      "Latitude": 42.2253579,
+      "Longitude": -8.63275039999999,
+      "Total": "1136157",
+      "Cambio2022": "0.19",
+      "Cambio2019": "0.12"
+    },
+    {
+      "AEROPUERTOS": "Reus",
+      "Latitude": 41.1483005,
+      "Longitude": 1.1683053,
+      "Total": 1045419,
+      "Cambio2022": 0.15,
+      "Cambio2019": 0.00
+    },
+    {
+      "AEROPUERTOS": "Federico García Lorca Granada-Jaén",
+      "Latitude": 37.1877458,
+      "Longitude": -3.7779569,
+      "Total": 1039429,
+      "Cambio2022": 0.14,
+      "Cambio2019": -0.17
+    },
+    {
+      "AEROPUERTOS": "Jerez de la Frontera",
+      "Latitude": 36.7433582,
+      "Longitude": -6.0652725,
+      "Total": 904,
+      "Cambio2022": -0.01,
+      "Cambio2019": -0.19
+    },
+    {
+      "AEROPUERTOS": "Región de Murcia",
+      "Latitude": 38.1398141,
+      "Longitude": -1.366216,
+      "Total": 877796,
+      "Cambio2022": 0.05,
+      "Cambio2019": -0.20
+    },
+    {
+      "AEROPUERTOS": "Almería",
+      "Latitude": 36.8447211,
+      "Longitude": -2.3704731,
+      "Total": 775393,
+      "Cambio2022": 0.10,
+      "Cambio2019": -0.21
+    },
+    {
+      "AEROPUERTOS": "Zaragoza",
+      "Latitude": 41.662242,
+      "Longitude": -1.0231211,
+      "Total": 685690,
+      "Cambio2022": 0.09,
+      "Cambio2019": 0.47
+    },
+    {
+      "AEROPUERTOS": "Melilla",
+      "Latitude": 35.2796766,
+      "Longitude": -2.9568598,
+      "Total": 501069,
+      "Cambio2022": 0.12,
+      "Cambio2019": 0.15
+    },
+    {
+      "AEROPUERTOS": "San Sebastián",
+      "Latitude": 43.3563455,
+      "Longitude": -1.7912035,
+      "Total": 482662,
+      "Cambio2022": 0.26,
+      "Cambio2019": 0.51
+    },
+    {
+      "AEROPUERTOS": "Vitoria",
+      "Latitude": 42.8834064,
+      "Longitude": -2.7312197,
+      "Total": 309929,
+      "Cambio2022": 0.32,
+      "Cambio2019": 0.78
+    },
+    {
+      "AEROPUERTOS": "El Hierro",
+      "Latitude": 27.814067,
+      "Longitude": -17.884965,
+      "Total": 301241,
+      "Cambio2022": 0.10,
+      "Cambio2019": 0.12
+    },
+    {
+      "AEROPUERTOS": "Valladolid",
+      "Latitude": 41.7060144,
+      "Longitude": -4.845069,
+      "Total": 208923,
+      "Cambio2022": 0.21,
+      "Cambio2019": -0.16
+    },
+    {
+      "AEROPUERTOS": "Pamplona",
+      "Latitude": 42.7676289,
+      "Longitude": -1.6391822,
+      "Total": 197509,
+      "Cambio2022": 0.14,
+      "Cambio2019": -0.19
+    },
+    {
+      "AEROPUERTOS": "La Gomera",
+      "Latitude": 28.0318862,
+      "Longitude": -17.2121376,
+      "Total": 113318,
+      "Cambio2022": 0.19,
+      "Cambio2019": 0.46
+    },
+    {
+      "AEROPUERTOS": "Ceuta (helipuerto)",
+      "Latitude": 35.8923321,
+      "Longitude": -5.30609539999999,
+      "Total": 87024,
+      "Cambio2022": 0.09,
+      "Cambio2019": 0.21
+    },
+    {
+      "AEROPUERTOS": "Badajoz",
+      "Latitude": 38.8939545,
+      "Longitude": -6.8188174,
+      "Total": 80181,
+      "Cambio2022": 0.22,
+      "Cambio2019": 0.06
+    },
+    {
+      "AEROPUERTOS": "León",
+      "Latitude": 42.5901953,
+      "Longitude": -5.6430764,
+      "Total": 63442,
+      "Cambio2022": 0.41,
+      "Cambio2019": -0.04
+    },
+    {
+      "AEROPUERTOS": "Algeciras (helipuerto)",
+      "Latitude": 36.1287021,
+      "Longitude": -5.4412162,
+      "Total": 41308,
+      "Cambio2022": 0.01,
+      "Cambio2019": 0.09
+    },
+    {
+      "AEROPUERTOS": "Salamanca",
+      "Latitude": 40.9439828,
+      "Longitude": -5.50191909999999,
+      "Total": 21083,
+      "Cambio2022": 0.62,
+      "Cambio2019": 0.19
+    },
+    {
+      "AEROPUERTOS": "Logroño",
+      "Latitude": 42.4558733,
+      "Longitude": -2.3148855,
+      "Total": 16728,
+      "Cambio2022": 0.41,
+      "Cambio2019": -0.14
+    },
+    {
+      "AEROPUERTOS": "Son Bonet",
+      "Latitude": 39.6030662,
+      "Longitude": 2.7058242,
+      "Total": 10061,
+      "Cambio2022": 0.12,
+      "Cambio2019": 1.44
+    },
+    {
+      "AEROPUERTOS": "Sabadell",
+      "Latitude": 41.5211639,
+      "Longitude": 2.1016878,
+      "Total": 6207,
+      "Cambio2022": 0.02,
+      "Cambio2019": 0.22
+    },
+    {
+      "AEROPUERTOS": "Córdoba",
+      "Latitude": 37.8444895,
+      "Longitude": -4.8436708,
+      "Total": 5938,
+      "Cambio2022": 0.79,
+      "Cambio2019": -0.45
+    },
+    {
+      "AEROPUERTOS": "Burgos",
+      "Latitude": 42.3548521,
+      "Longitude": -3.62281679999999,
+      "Total": 4053,
+      "Cambio2022": 0.93,
+      "Cambio2019": -0.77
+    },
+    {
+      "AEROPUERTOS": "Albacete",
+      "Latitude": 38.9453785,
+      "Longitude": -1.880496,
+      "Total": 2644,
+      "Cambio2022": 2.09,
+      "Cambio2019": 0.64
+    },
+    {
+      "AEROPUERTOS": "Madrid-Cuatro Vientos",
+      "Latitude": 40.3685237,
+      "Longitude": -3.7806676,
+      "Total": 1956,
+      "Cambio2022": -0.12,
+      "Cambio2019": -0.45
+    },
+    {
+      "AEROPUERTOS": "Huesca-Pirineos",
+      "Latitude": 42.0823208,
+      "Longitude": -0.3227301,
+      "Total": 276,
+      "Cambio2022": -0.15,
+      "Cambio2019": -0.56
+    }
+  ]
